2f695aeee53abe5fa21ac2cd121944869c52df1e
Yan Doldonov <yd@maviray.com>
2026-01-27 23:05:02 +0100

    spinner effect in MaviButton during onclick execution

--- src/Maviray.Blazor.Components.Material/Components/Inputs/MaviButtonBase.cs
+++ src/Maviray.Blazor.Components.Material/Components/Inputs/MaviButtonBase.cs
@@ -3,6 +3,7 @@ using Maviray.Blazor.Components.Core.Enums;
 using Maviray.Blazor.Components.Core.EventArgs;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Logging;
 
 namespace Maviray.Blazor.Components.Material.Components.Inputs
 {
@@ -26,31 +27,50 @@ namespace Maviray.Blazor.Components.Material.Components.Inputs
 
         protected string Role => ButtonRole.ToString().ToLower();
 
+        protected bool IsLoading { get; set; } = false;
+
         protected async Task HandleButtonClick(MouseEventArgs args)
         {
             if (OnClick.HasDelegate)
             {
-                var customArgs = new MouseClickEventArgs(Id)
+                if (IsLoading)
                 {
-                    AltKey = args.AltKey,
-                    Button = args.Button,
-                    Buttons = args.Buttons,
-                    ClientX = args.ClientX,
-                    ClientY = args.ClientY,
-                    CtrlKey = args.CtrlKey,
-                    Detail = args.Detail,
-                    MetaKey = args.MetaKey,
-                    OffsetX = args.OffsetX,
-                    OffsetY = args.OffsetY,
-                    PageX = args.PageX,
-                    PageY = args.PageY,
-                    ScreenX = args.ScreenX,
-                    ScreenY = args.ScreenY,
-                    ShiftKey = args.ShiftKey,
-                    Type = args.Type
-                };
+                    Logger?.LogInformation("delegate executing. will quit.");
+                    return;
+                }
 
-                await OnClick.InvokeAsync(customArgs);
+                IsLoading = true;
+                StateHasChanged();
+
+                try
+                {
+                    var customArgs = new MouseClickEventArgs(Id)
+                    {
+                        AltKey = args.AltKey,
+                        Button = args.Button,
+                        Buttons = args.Buttons,
+                        ClientX = args.ClientX,
+                        ClientY = args.ClientY,
+                        CtrlKey = args.CtrlKey,
+                        Detail = args.Detail,
+                        MetaKey = args.MetaKey,
+                        OffsetX = args.OffsetX,
+                        OffsetY = args.OffsetY,
+                        PageX = args.PageX,
+                        PageY = args.PageY,
+                        ScreenX = args.ScreenX,
+                        ScreenY = args.ScreenY,
+                        ShiftKey = args.ShiftKey,
+                        Type = args.Type
+                    };
+
+                    await OnClick.InvokeAsync(customArgs);
+                }
+                finally
+                {
+                    IsLoading = false;
+                    StateHasChanged();
+                }
             }
         }
     }
