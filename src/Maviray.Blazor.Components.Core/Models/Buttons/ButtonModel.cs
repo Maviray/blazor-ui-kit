d043f801085d862210f1eb069b405501dc49eb95
Yan Doldonov <yd@maviray.com>
2026-01-27 08:28:13 +0100

    button group draft

---
+++ src/Maviray.Blazor.Components.Core/Models/Buttons/ButtonModel.cs
@@ -0,0 +1,31 @@
+﻿using Maviray.Blazor.Components.Core.Enums;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Maviray.Blazor.Components.Core.Models.Buttons
+{
+    public class ButtonModel
+    {
+        public string? Id { get; set; } = $"button_{Guid.NewGuid()}";
+
+        public ButtonRole ButtonRole { get; set; }
+
+        public ButtonType ButtonType { get; set; }
+
+        public ElementSize ElementSize { get; set; }
+
+        public TextTransform TextTransform { get; set; }
+
+        public ButtonVariant ButtonVariant { get; set; }
+
+        public bool Disabled { get; set; }
+
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        public string Width { get; set; } = "min-w-16";
+
+        public string? StartIcon { get; set; }
+
+        public string? EndIcon { get; set; }
+    }
+}
