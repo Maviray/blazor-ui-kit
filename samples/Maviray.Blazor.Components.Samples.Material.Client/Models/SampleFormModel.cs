e704bfda0c63a26f1461a299bab0fe9814812e13
Yan Doldonov <yd@maviray.com>
2026-02-03 07:34:16 +0100

    showcase MaviInputInteger

--- samples/Maviray.Blazor.Components.Samples.Material.Client/Models/SampleFormModel.cs
+++ samples/Maviray.Blazor.Components.Samples.Material.Client/Models/SampleFormModel.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Maviray.Blazor.Components.Samples.Material.Client.Models;
 
