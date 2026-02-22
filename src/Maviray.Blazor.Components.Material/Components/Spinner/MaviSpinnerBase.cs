4c465d5d2a6170cc6335d346fabbdb2ee8976313
Yan Doldonov <yd@maviray.com>
2026-02-22 23:16:44 +0100

    Spinners

--- src/Maviray.Blazor.Components.Material/Components/Spinner/MaviSpinnerBase.cs
+++ src/Maviray.Blazor.Components.Material/Components/Spinner/MaviSpinnerBase.cs
@@ -1,8 +1,11 @@
 ﻿using Maviray.Blazor.Components.Core.Components;
+using Maviray.Blazor.Components.Core.Enums;
+using Microsoft.AspNetCore.Components;
 
 namespace Maviray.Blazor.Components.Material.Components.Spinner;
 
 public class MaviSpinnerBase : MaviComponentBase
 {
-    
+    [Parameter] public ElementSize ElementSize { get; set; } = ElementSize.Regular;
+    [Parameter] public ThemeColorScheme ThemeColorScheme { get; set; } = ThemeColorScheme.Default;
 }
